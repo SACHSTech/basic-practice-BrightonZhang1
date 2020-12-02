e2fc468f24254c8c92bb7fcb0e4b60256ee53b10
Brighton Zhang <brighton.zhang23@ycdsbk12.ca>
2020-12-02 19:53:32 +0000

    completed practice 1

---
+++ avg_marks.py
@@ -0,0 +1,9 @@
+
+mark1 = float(input("Enter mark 1: "))
+mark2 = float(input("Enter mark 2: "))
+mark3 = float(input("Enter mark 3: "))
+mark4 = float(input("Enter mark 4: "))
+
+average =(mark1 + mark2 + mark3 + mark4)/4
+
+print("The average of the 4 marks is", (average))
